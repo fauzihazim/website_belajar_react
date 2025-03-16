62c4fd7e2d26acd1c279b1dd19579b80e031a165
fauzihazim <fauzihazim28@gmail.com>
2025-03-16 11:02:37 +0700

    add: README.md

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# Getting Started with Create React App
+# Website Belajar React
+
+Web ini dibangun dengan react js dan mengkonsumsi api dari MockAPI. Web ini sebagai sarana pembelajaran komunikasi antara front-end (React Js) dengan API.
+
+## Tampilan
+| Tampilan desktop | Tampilan mobile | Nama halaman | URL | Keterangan |
+|:-----|:--------:|------:|------:|------:|
+| ![Halaman Desktop Login](https://s6.imgcdn.dev/Yj65gN.png) | ![Halaman Mobile Login](https://s6.imgcdn.dev/Yj6PNq.png) | Halaman Login | http://localhost:3000/ | Pada user untuk login sesuai dengan emailnya |
+| ![Halaman Desktop Beranda](https://s6.imgcdn.dev/Yj6TGB.png) | ![Halaman Mobile Beranda](https://s6.imgcdn.dev/Yj6Ueu.png) | Halaman Beranda | http://localhost:3000/beranda | Pada halaman ini menampilkan daftar course |
+| ![Halaman Desktop Register](https://s6.imgcdn.dev/Yj6zML.png) | ![Halaman Mobile Register](https://s6.imgcdn.dev/Yj6aza.png) | Halaman Register | http://localhost:3000/register | Pada halaman untuk menambah user |
+| ![Halaman Desktop Forgot Password](https://media-hosting.imagekit.io//53edae35786c4941/screenshot_1742096862468.png?Expires=1836704864&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=2PsjUJNgnmdLpd-2pBGaTL1G0U0iqJIoCJej-Q84WVI9Fa6cjkF2tUjyuUw9CKkBiMx8b5SVsCGJoG~e4oPDt3JG2RdugeZ8okS1tn6fz1i28RlCCmx9CWb~shhYhMo1IwC8IW3Ie4715TfH7fdTae8optLWHFHU7oNdXEQ9GL1o1KB2fsyKxIMz702D9WgkdA0RkIrP5aJxG-xb4a9VJ1NMMwBYHW1KNge9pleST-0TBw4zQJfPh7B9BvshB~oSyL5S-Pn2AU8m1EK1YMLF96QmQTcF4r~EltSWFxx70Jr3soB198h0W0Cugwf~wBuKa8fb24c1brgxvuLJCXgBSw__) | ![Halaman Mobile Forgot Password](https://media-hosting.imagekit.io//53edae35786c4941/screenshot_1742096862468.png?Expires=1836704864&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=2PsjUJNgnmdLpd-2pBGaTL1G0U0iqJIoCJej-Q84WVI9Fa6cjkF2tUjyuUw9CKkBiMx8b5SVsCGJoG~e4oPDt3JG2RdugeZ8okS1tn6fz1i28RlCCmx9CWb~shhYhMo1IwC8IW3Ie4715TfH7fdTae8optLWHFHU7oNdXEQ9GL1o1KB2fsyKxIMz702D9WgkdA0RkIrP5aJxG-xb4a9VJ1NMMwBYHW1KNge9pleST-0TBw4zQJfPh7B9BvshB~oSyL5S-Pn2AU8m1EK1YMLF96QmQTcF4r~EltSWFxx70Jr3soB198h0W0Cugwf~wBuKa8fb24c1brgxvuLJCXgBSw__) | Halaman forgot password | http://localhost:3000/lupaKataSandi | Pada halaman ini memungkinkan merubah password |
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/pages/lupaKataSandi/LupaKataSandi.js
+++ src/pages/lupaKataSandi/LupaKataSandi.js
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { useMediaQuery } from "@uidotdev/usehooks";
 // import useLink from '../../data/UseLink';
+import './LupaKataSandi.css';
 import useLink from '../../store/zustand/Store';
 import { useNavigate } from 'react-router-dom';
 import { GetData, UpdatePassword } from '../../services/api';
@@ -8,11 +10,12 @@ const LupaKataSandi = () => {
     const navigate = useNavigate();
     const users = useLink((state) => state.users);
     const getAllUsers = useLink((state) => state.getAllUsers);
+    const bigScreenDevice = useMediaQuery("only screen and (min-width : 811px)");
 
-    const selectedUser = useLink((state) => state.selectedUser);
+    // const selectedUser = useLink((state) => state.selectedUser);
     const selectUserByEmail = useLink((state) => state.selectUserByEmail);
-    const linkGet = useLink((state) => state.linkGet);
-    const linkUpdate = useLink((state) => state.linkUpdate);
+    // const linkGet = useLink((state) => state.linkGet);
+    // const linkUpdate = useLink((state) => state.linkUpdate);
     // const [ users, setUsers ] = useState([]);
     const [ email, setEmail ] = useState('');
     const [ kataSandi, setKataSandi ] = useState('');
@@ -39,16 +42,95 @@ const LupaKataSandi = () => {
         navigate('/beranda');
     }
 
+    function seePassword () {
+        const togglePassword = document.getElementById('togglePassword');
+        const password = document.getElementById('kataSandi');
+    
+        if (togglePassword && password) {
+            if (password.type === "password") {
+                password.type = "text";
+                togglePassword.classList.remove("fa-eye");
+                togglePassword.classList.add("fa-eye-slash");
+            } else {
+                password.type = "password";
+                togglePassword.classList.remove("fa-eye-slash");
+                togglePassword.classList.add("fa-eye");
+            }
+        }
+    }
+
+    function seeConfirmPassword () {
+        const toggleConfirmPassword = document.getElementById('toggleConfirmPassword');
+        const password = document.getElementById('konfirmasiKataSandi');
+    
+        if (toggleConfirmPassword && password) {
+            if (password.type === "password") {
+                password.type = "text";
+                toggleConfirmPassword.classList.remove("fa-eye");
+                toggleConfirmPassword.classList.add("fa-eye-slash");
+            } else {
+                password.type = "password";
+                toggleConfirmPassword.classList.remove("fa-eye-slash");
+                toggleConfirmPassword.classList.add("fa-eye");
+            }
+        }
+    }
+
     return (
-        <div>
-            <h1>Kata Sandi</h1>
-            <label htmlFor="email">Email</label><br />
-            <input type="email" name="" id="email" value={email} onChange={handleEmail}/><br />
-            <label htmlFor="kataSandi">Kata Sandi Baru</label><br />
-            <input type="password" name="kata sandi baru" id="kataSandi" value={kataSandi} onChange={handleKataSandi} /><br />
-            <label htmlFor="konfirmasiKataSandi">Konfirmasi Kata Sandi Baru</label><br />
-            <input type="password" name="konfirmasi kata sandi baru" id="konfirmasiKataSandi" value={konfirmasiKataSandi} onChange={handleKonfirmasiKataSandi} /><br />
-            <button onClick={updateKataSandi}>Submit</button><br />
+        <div className={bigScreenDevice ? "isBigScreen" : "isSmallScreen"}>
+            <article>
+                <h1>Forget Password</h1>
+                <label htmlFor="email" className='labelEmail'>Email</label><br />
+                <input type="email" className='inputEmail' name="" id="email" value={email} onChange={handleEmail}/><br />
+                {/* <div className="user-box">
+                    <label htmlFor="kataSandi" className='labelPassword'>Kata Sandi Baru <span className="required">*</span></label><br />
+                    <input type="password" name="kata sandi baru" className='inputPassword' id="kataSandi" value={kataSandi} onChange={handleKataSandi} /><br />
+                    <span className="password-toggle-icon">
+                        <i className="fa-solid fa-eye" id="togglePassword" onClick={() => seePassword()}></i>
+                    </span>
+                </div> */}
+                <div className="user-box" style={{ position: "relative" }}>
+                    <label htmlFor="kataSandi" className="labelPassword">
+                        Kata Sandi Baru <span className="required">*</span>
+                    </label>
+                    <br />
+                    <input
+                        type="password"
+                        name="kata sandi baru"
+                        className="inputPassword"
+                        id="kataSandi"
+                        value={kataSandi}
+                        onChange={handleKataSandi}
+                    />
+                    <i
+                        className="fa-solid fa-eye password-toggle-icon"
+                        id="togglePassword"
+                        onClick={() => seePassword()}
+                    ></i>
+                </div>
+                {/* <label htmlFor="konfirmasiKataSandi" className='labelConfirmPassword'>Konfirmasi Kata Sandi Baru</label><br />
+                <input type="password" name="konfirmasi kata sandi baru"  className='inputConfirmPassword' id="konfirmasiKataSandi" value={konfirmasiKataSandi} onChange={handleKonfirmasiKataSandi} /><br /> */}
+                <div className="user-box" style={{ position: "relative" }}>
+                    <label htmlFor="konfirmasiKataSandi" className="labelConfirmPassword">
+                        Konfirmasi Kata Sandi Baru<span className="required">*</span>
+                    </label>
+                    <br />
+                    <input
+                        type="password"
+                        name="konfirmasi kata sandi baru"
+                        className="inputKonfirmasiPassword"
+                        id="konfirmasiKataSandi"
+                        value={konfirmasiKataSandi}
+                        onChange={handleKonfirmasiKataSandi}
+                    />
+                    <i
+                        className="fa-solid fa-eye password-toggle-icon"
+                        id="toggleConfirmPassword"
+                        onClick={() => seeConfirmPassword()}
+                    ></i>
+                </div>
+                <button className='btnSubmit' onClick={updateKataSandi}>Submit</button><br />
+            </article>
         </div>
     );
 };
